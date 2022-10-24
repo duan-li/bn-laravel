84041faf21d84767941c926db48867a84aecfb28
Duan Li <duan@MacBook-Max.local>
2022-10-24 16:04:12 +1100

    setup passport

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('ready', function (){
+    return 'ready';
+});
+
+Route::prefix('v1')->middleware('auth:api')
+        ->group(function () {
+    Route::get('home', function (){
+        return 'ok';
+    });
+});
